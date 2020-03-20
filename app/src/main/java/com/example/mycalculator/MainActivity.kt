19b335a669c84a7f51b8487cb34db3a78b310dfd
faseehahmad00@gmail.com <FASEEH21100@f>
2020-03-20 17:04:18 +0500

    added evaluation function and changed name,icon

--- app/src/main/java/com/example/mycalculator/MainActivity.kt
+++ app/src/main/java/com/example/mycalculator/MainActivity.kt
@@ -16,104 +16,124 @@ class MainActivity : AppCompatActivity() {
         var screen: String = " "
 
         button.setOnClickListener {
-           if(screen.length < 15){
-            screen = (screen + button.text)
-            textView.text = screen
-           }
+            if (screen.length < 50) {
+                screen = (screen + button.text)
+                textView.text = screen
+                val result = expression.eval(screen)
+                textView2.text = "= " + result.toString()
+            }
         }
         button2.setOnClickListener {
-            if (screen.length < 15) {
+            if (screen.length < 50) {
                 screen = (screen + button2.text)
                 textView.text = (screen)
+                val result = expression.eval(screen)
+                textView2.text =  "= " + result.toString()
             }
         }
         button3.setOnClickListener {
-            if (screen.length < 15) {
+            if (screen.length < 50) {
                 screen = (screen + button3.text)
                 textView.text = screen
+                val result = expression.eval(screen)
+                textView2.text = "= " + result.toString()
             }
         }
         button4.setOnClickListener {
-            if (screen.length < 15) {
+            if (screen.length < 50) {
                 screen = (screen + button4.text)
                 textView.text = screen
+                val result = expression.eval(screen)
+                textView2.text = "= " + result.toString()
             }
         }
         button5.setOnClickListener {
-            if (screen.length < 15) {
+            if (screen.length < 50) {
                 screen = (screen + button5.text)
                 textView.text = screen
+                val result = expression.eval(screen)
+                textView2.text = "= " + result.toString()
             }
         }
         button6.setOnClickListener {
-            if (screen.length < 15) {
+            if (screen.length < 50) {
                 screen = (screen + button6.text)
                 textView.text = screen
+                val result = expression.eval(screen)
+                textView2.text =  "= " + result.toString()
             }
         }
         button7.setOnClickListener {
-            if (screen.length < 15) {
+            if (screen.length < 50) {
                 screen = (screen + button7.text)
                 textView.text = screen
+                val result = expression.eval(screen)
+                textView2.text =  "= " + result.toString()
             }
         }
         button8.setOnClickListener {
-            if (screen.length < 15) {
+            if (screen.length < 50) {
                 screen = (screen + button8.text)
                 textView.text = screen
+                val result = expression.eval(screen)
+                textView2.text =  "= " + result.toString()
             }
         }
         button9.setOnClickListener {
-            if (screen.length < 15) {
+            if (screen.length < 50) {
                 screen = (screen + button9.text)
                 textView.text = screen
+                val result = expression.eval(screen)
+                textView2.text =  "= " + result.toString()
             }
         }
         button10.setOnClickListener {
-            if (screen.length < 15) {
+            if (screen.length < 50) {
                 screen = (screen + button10.text)
                 textView.text = screen
             }
         }
         button11.setOnClickListener {
-            if (screen.length < 15) {
+            if (screen.length < 50) {
                 screen = (screen + button11.text)
                 textView.text = screen
             }
         }
         button12.setOnClickListener {
-            if (screen.length < 15) {
+            if (screen.length < 50) {
                 screen = (screen + "*")
                 textView.text = screen
             }
         }
         button13.setOnClickListener {
-            if (screen.length < 15) {
+            if (screen.length < 50) {
                 screen = (screen + button13.text)
                 textView.text = screen
             }
         }
 
         button14.setOnClickListener {
-            if (screen.length < 15) {
+            if (screen.length < 50) {
                 screen = (screen + button14.text)
                 textView.text = screen
+                val result = expression.eval(screen)
+                textView2.text =  "= " + result.toString()
             }
         }
         button16.setOnClickListener {
-            if (screen.length < 15) {
+            if (screen.length < 50) {
                 screen = (screen + button16.text)
                 textView.text = screen
             }
         }
         button18.setOnClickListener {
-            if (screen.length < 15) {
+            if (screen.length < 50) {
                 screen = (screen + button18.text)
                 textView.text = screen
             }
         }
         button19.setOnClickListener {
-            if (screen.length < 15) {
+            if (screen.length < 50) {
                 screen = (screen + button19.text)
                 textView.text = screen
             }
@@ -126,47 +146,53 @@ class MainActivity : AppCompatActivity() {
             if (screen.length >= 1) {
                 screen = screen.substring(0, (screen.length - 1))
                 textView.text = screen
+                try {
+                    val result = expression.eval(screen)
+                    textView2.text = result.toString()
+                } catch (e: Exception) {
+                    textView2.text = ""
+                }
             }
         }
         button15.setOnClickListener {
             try {
                 val result = expression.eval(screen)
-                textView2.text = result.toString()
+                textView2.text = "= " + result.toString()
                 screen = ""
                 textView.text = ""
 
             } catch (e: Exception) {
-                textView2.text = "error"
+                textView2.text = "E"
                 screen = ""
             }
 
         }
         button20.setOnClickListener {
-            if (screen.length < 15) {
+            if (screen.length < 50) {
                 screen = (screen + button20.text)
                 textView.text = screen
             }
         }
         button21.setOnClickListener {
-            if (screen.length < 15) {
+            if (screen.length < 50) {
                 screen = (screen + button21.text)
                 textView.text = screen
             }
         }
         button22.setOnClickListener {
-            if (screen.length < 15) {
+            if (screen.length < 50) {
                 screen = (screen + button22.text)
                 textView.text = screen
             }
         }
         button23.setOnClickListener {
-            if (screen.length < 15) {
+            if (screen.length < 50) {
                 screen = (screen + button23.text)
                 textView.text = screen
             }
         }
         button24.setOnClickListener {
-            if (screen.length < 15) {
+            if (screen.length < 50) {
                 screen = (screen + button24.text)
                 textView.text = screen
             }
